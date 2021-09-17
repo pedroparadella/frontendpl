ebdd0545c31b9e1f5e5002dcd75b4392257b7fbb
Cauê Melo <cauesmelo@gmail.com>
2021-09-17 19:51:02 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h2>Poké Explorer</h2>
+      <GlobalStyle />
     </div>
   );
 }
