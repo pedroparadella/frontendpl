a39285ef196cbd23befb0ae9b1afa7d13e474938
Cauê Melo <cauesmelo@gmail.com>
2021-09-17 20:32:52 -0300

    chore: add eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { GlobalStyle } from "./styles/global";
+import { GlobalStyle } from './styles/global';
 
-export function App() {
+export const App = () => {
   return (
     <div className="App">
       <h2>Poké Explorer</h2>
       <GlobalStyle />
     </div>
   );
-}
-
+};
