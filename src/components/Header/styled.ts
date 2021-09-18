8b172e2710133fc8f6052499078110d03c2c1893
Cauê Melo <cauesmelo@gmail.com>
2021-09-18 00:04:20 -0300

    fix: adjust header position

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -7,6 +7,8 @@ export const LogoWrapper = styled.div`
 `;
 
 export const Header = styled.header`
+  position: fixed;
+  top: 0;
   width: 100%;
   display: flex;
   justify-content: center;
--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  padding-top: 4rem;
   display: flex;
   flex-direction: column;
   align-items: center;
