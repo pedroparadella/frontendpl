4276a1faf4271d5bcb04d45bf1822616c7c99ca1
Cauê Melo <cauesmelo@gmail.com>
2021-09-17 23:07:35 -0300

    feat: add search component

--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  max-width: var(--max-width-content);
+`;
+
+export const MainTitleWrapper = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CardsWrapper = styled.article`
+  border: 1px solid red;
+`;
