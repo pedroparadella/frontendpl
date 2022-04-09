ab26a66858c20d971d3ad3c746512ffe8b3f7718
vonmalves <ismael.only@gmail.com>
2022-04-09 14:39:18 -0300

    feat: component PokemonCard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,76 @@
+import { Button, Divider, Flex, Icon, Image, Text } from '@chakra-ui/react';
+
+import { RiDeleteBinLine, RiPencilLine } from 'react-icons/ri';
+import { useDrawerForm } from '../contexts/DrawerFormContext';
+
+import { PokemonType } from '../types/pokemon.type';
+
+export function PokemonCard({ name, sprites }: any) {
+  const { onOpen } = useDrawerForm();
+
+  return (
+    <Flex
+      direction='column'
+      mt='9'
+      maxW='60'
+      h='72'
+      bgColor='white'
+      borderRadius='md'
+      border='1px'
+      borderColor='gray.200'
+      shadow='md'
+    >
+      <Image
+        src={sprites}
+        borderRadius='full'
+        bgColor='gray.200'
+        border='2px'
+        borderColor='gray.300'
+        boxSize='95px'
+        alt={name}
+        alignSelf='center'
+        p='2'
+        my='12'
+      />
+      <Divider
+        orientation='horizontal'
+        alignSelf='center'
+        w='170px'
+        mb='10px'
+        borderColor='gray.300'
+      />
+      <Text textAlign='center'>
+        {name}
+      </Text>
+      <Flex bg='gray.200' borderBottomRadius='md' marginTop='auto'>
+        <Button bgColor='transparent' color='gray.600'>
+          <Icon
+            as={RiDeleteBinLine}
+            color='red.500'
+            fontSize='xl'
+            mr='3'
+            mb='1'
+          />
+          Excluir
+        </Button>
+        <Divider
+          orientation='vertical'
+          alignSelf='center'
+          h='22px'
+          borderColor='gray.300'
+        />
+        <Button bgColor='transparent' color='gray.600' onClick={onOpen}>
+          <Icon
+            as={RiPencilLine}
+            color='orange.500'
+            fontSize='xl'
+            mr='3'
+            mb='1'
+          />
+          Editar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
+
